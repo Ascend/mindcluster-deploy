49e97a4d05d520385d4f9e1c40c27ceb19ae42cd
liujp20 <3058705262@qq.com>
2025-02-06 07:52:21 +0000

    update npu_collector/npu_info_collect.sh. 脚本兼容，旧版本工具也能使用新脚本采集结果诊断
    Signed-off-by: liujp20 <3058705262@qq.com>

--- npu_collector/npu_info_collect.sh
+++ npu_collector/npu_info_collect.sh
@@ -128,6 +128,14 @@ do
 done
 
 
+for((i=0;i<npu_num;i++));
+do
+  echo "/usr/local/Ascend/driver/tools/hccn_tool -i ${i} -optical -g | grep prese">>${save_file}
+  /usr/local/Ascend/driver/tools/hccn_tool -i ${i} -optical -g | grep prese>>${save_file}
+  echo -e "\n">>${save_file}
+done
+
+
 for((i=0;i<hccn_tool_num;i++));
 do
   echo "/usr/local/Ascend/driver/tools/hccn_tool -i ${i} -optical -g">>"${save_file}"
