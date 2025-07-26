badd1c4431885bfb1078bd52b91f9237b3d0afb6
lirui238 <2396601465@qq.com>
2025-07-26 12:51:14 +0800

    移除rc kubeconfig

--- conf/createRcRole.sh
+++ conf/createRcRole.sh
@@ -15,41 +15,6 @@
 
 # run this shell like this : bash createKubeConfig.sh https://masterip:port
 set -e
-if [ $# = 0 ]; then
-  echo "please input the kube-apiserver ip, like https://127.0.0.1:6443"
-  exit 0
-fi
-
-function createCRT() {
-  cd /etc/kubernetes/mindxdl
-  openssl genrsa -out $1.key 2048
-  openssl req -new -key $1.key -out $1.csr -subj "/CN=$1/O=k8s"
-  openssl x509 -req -in $1.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out $1.crt -days 1000
-
-}
-
-function createKubeConfig() {
-  createCRT $2
-  kubectl config set-cluster kubernetes \
-  --certificate-authority=/etc/kubernetes/pki/ca.crt \
-  --embed-certs=true \
-  --server=$1 \
-  --kubeconfig=$2-cfg.conf
-
-  kubectl config set-credentials $2 \
-  --client-certificate=/etc/kubernetes/mindxdl/$2.crt \
-  --client-key=/etc/kubernetes/mindxdl/$2.key \
-  --embed-certs=true \
-  --kubeconfig=$2-cfg.conf
-
-  kubectl config set-context $2-context \
-  --cluster=kubernetes \
-  --user=$2 \
-  --kubeconfig=$2-cfg.conf
-
-  kubectl config use-context $2-context --kubeconfig=$2-cfg.conf
-
-}
 
 function createRoleBinding() {
   #resilience-controller
@@ -62,21 +27,6 @@ function createRole() {
   #resilience-controller
   kubectl delete clusterrole resilience-controller-role || true
   creatRCRole
-}
-
-function init() {
-  cd /etc/kubernetes/
-  rm -rf /etc/kubernetes/mindxdl
-  mkdir /etc/kubernetes/mindxdl
-  chmod 750 /etc/kubernetes/mindxdl
-  cd mindxdl
-}
-
-function  clean() {
-    cd /etc/kubernetes/mindxdl
-    rm -rf *.key
-    rm -rf *.crt
-    rm -rf *.csr
 }
 
 function creatRCRole() {
@@ -101,14 +51,7 @@ rules:
 EOF
 }
 
-echo "start to create kubeconfig files for MindXDL"
-init
-createKubeConfig $1 resilience-controller
-clean
-echo "kubeconfig files create successfully"
 createRole
 echo "clusterrole create successfully"
 createRoleBinding
 echo "createRoleBinding create successfully"
-echo "finish! config file is in $(pwd)"
-ls -al $(pwd)
